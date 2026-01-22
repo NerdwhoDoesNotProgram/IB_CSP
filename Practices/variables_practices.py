8eafbe3ee4d73cb39817b5b6f32daa00864ab562
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-01-22 14:31:58 -0700

    finished notes and first_program

--- Practices/variables_practices.py
+++ Practices/variables_practices.py
@@ -1,3 +1,25 @@
 # IB Variables Practice
 # every assignment has to have your initials, and name of assignment
 
+name = "Billy"
+
+number = 8
+
+big_number = 987
+
+breakfast = "toast"
+
+fav_color = "Blue"
+
+school_name = "The best one obviously, UCAS!"
+
+year = 2026
+
+eye_color = "Blue"
+
+age = 15
+
+fav_school_subject = "Geography"
+
+
+print(name, "chose the number", number, "and", big_number, ". They had", breakfast, "for breakfast before going to", school_name, "for the", year, "year of school.", name, "favorite")
