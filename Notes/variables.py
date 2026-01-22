773a5d7550a4106d3aee931c4213eafeede3199d
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-01-22 14:13:22 -0700

    variables Notes

--- Notes/variables.py
+++ Notes/variables.py
@@ -1,3 +1,17 @@
-name = "Isaac"
+name = input("What is your name: ")
 
 print(name)
+
+room = 201
+
+subject = "computer Science"
+
+pi = 3.14
+
+money = -99999999999999999
+
+print(name)
+
+print("Computer Science is the best")
+
+print("Ms. LaRose teaches", subject, "in room", room)
---
+++ Practices/variables_practices.py
@@ -0,0 +1,3 @@
+# IB Variables Practice
+# every assignment has to have your initials, and name of assignment
+
