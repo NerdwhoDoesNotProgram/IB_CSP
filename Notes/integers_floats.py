3c39cf959d2ee74d61ee849481de95e0b0681c37
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-01-23 14:42:18 -0700

    Numbers Notes

---
+++ Notes/integers_floats.py
@@ -0,0 +1,13 @@
+"""price = float( input("How much does it cost: $"))
+
+sales_tax = float( input("How much is the sales tax: %"))/100
+
+total = rpund(price + (price * sales_tax), 2) # or I can do it like: total = round( price + tax, 2)
+
+print(total)"""
+
+apples = int(input("how many apples are there: "))
+
+friends = 8
+
+print("Each frind can have", apples//friends, ", And I have", apples%friends, "Left over")
