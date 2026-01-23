63070e28c2a1e0da06e9ca9206e2f7a044eeb736
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-01-23 14:09:33 -0700

    Message

--- Practices/variables_practices.py
+++ Practices/variables_practices.py
@@ -22,4 +22,4 @@ age = 15
 fav_school_subject = "Geography"
 
 
-print(name, "chose the number", number, "and", big_number, ". They had", breakfast, "for breakfast before going to", school_name, "for the", year, "year of school.", name, "favorite")
+print(name, "chose the number", number, "and", big_number, ". They had", breakfast, "for breakfast before going to", school_name, "for the", year, "year of school.", name, "favorite class is", fav_school_subject, "They have", eye_color, "eyes, and", fav_color, "Is their favorite color. How old are they, you might ask? Well, they are", age)
