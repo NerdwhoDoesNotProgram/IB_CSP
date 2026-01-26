835084cebdcdab0d893dba3f52fabe5f6428a6f0
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-01-26 14:42:19 -0700

    Financial Calculator nearly complete

--- Practices/financial_calculator.py
+++ Practices/financial_calculator.py
@@ -1,7 +1,29 @@
 # IB Finacial Calculator - Python
 
-monthly_income = input("How much do you make monthly: ")
+income = float(input("How much do you make monthly: $ "))
 
-rent = input("How much is your rent per month: ")
+rent = float(input("How much is your rent per month: $ "))
 
-utilities = input("How much is your utilities: ")
+utilities = float(input("How much is your utilities: $ "))
+
+groceries = float(input("How much do you pay for groceries: $ "))
+
+transportation = float(input("How much do you pay for transportation: $ "))
+
+savings = float(income*0.10)
+
+spending = float(income-rent-utilities-groceries-transportation-savings)
+
+
+
+print("Your rent is $", rent, "and that is", int((rent/income)*100), "%","of your income")
+
+print("Your utilities are $", utilities,"and that is", int((utilities/income)*100), "%", "of your income")
+
+print("Your Groceries are $", groceries, "and that is", int((groceries/income)*100), "%", "of your income")
+
+print("Your Transportation is $", transportation, "and that is", int((transportation/income)*100), "of your income")
+
+print("You should save $", savings,"a month, and that is 10 % of your income" )
+
+print("You have $", spending, "of sepending money each month!")
