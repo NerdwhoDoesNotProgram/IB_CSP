6704cc4671317d99ce01c28ecf2de04cc17d01f2
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-01-28 23:28:04 +0000

    added a space

--- Practices/silly_sentences.py
+++ Practices/silly_sentences.py
@@ -40,6 +40,6 @@ elif choice == "1":
     print("I spent " +  number1  + " hours " + verb + " to find why my code broke, only to discover that I typed a " +  punctuation_mark + " " + number2 + " times, instead of a variable name.")
 
 else: 
-    print("You chose" + choice + ", which wasn’t even an option. You know, after " + number1, "hours of staring at this code, I’m " + verb + " to think that", punctuation_mark, "on line", number2, "has better judgment than both of us.")
+    print("You chose " + choice + ", which wasn’t even an option. You know, after " + number1, "hours of staring at this code, I’m " + verb + " to think that", punctuation_mark, "on line", number2, "has better judgment than both of us.")
 
     
