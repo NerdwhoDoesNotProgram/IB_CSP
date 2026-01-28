9facb4081a8439ad67805c9f89de6faf7b098b37
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-01-28 14:17:25 -0700

    Silly sentences updated to folllow instructions

--- Practices/silly_sentences.py
+++ Practices/silly_sentences.py
@@ -1,19 +1,20 @@
-# IB Silly Sentences - Python
+# IB Period 7  Silly Sentences - Python
 
 
 choice = input("1 or 2: ")
 
-punctuation_mark = input("Type of punctuation mark: ")
-number1 = input("Number of hours: ")
+punctuation_mark = input("Type of punctuation mark (Spell it out): ").lower()
+number1 = input("Number of hours (Spell it out): ").lower()
+verb = input("Verb ending in ing: ").lower()
 number2 = input("Number between 1 and 100: ")
 
 
 
 if choice == "2": 
-    print("After", number1, "hours of debugging, I finally realized I was missing a", punctuation_mark, "on line", number2 + ".")
+    print("After" + " " + number1 + " " + "hours of" + " " + verb + ", " + "I finally realized I was missing a" + " " +  punctuation_mark + " " +  "on line" + " " +  number2 + ".")
 
 elif choice == "1":
-    print("I spent", number1, "hours trying to find why my code broke, only to discover I typed a", punctuation_mark, number2,"times, instead of a variable name.")
+    print("I spent", number1, "hours" + " " + verb + " " +  "to find why my code broke, only to discover I typed a" + " " +  punctuation_mark + " " + number2 + " " + "times, instead of a variable name.")
 
 else: 
-    print("You chose ", choice,", which wasn’t even an option. You know, after", number1, "hours of staring at this code, I’m starting to think that", punctuation_mark, "on line", number2, "has better judgment than both of us.")
+    print("You chose" + " " + choice + ", " +  "which wasn’t even an option. You know, after" + " " + number1, "hours of staring at this code, I’m" + " " + verb + " " +  "to think that", punctuation_mark, "on line", number2, "has better judgment than both of us.")
