756e7d2b28e1e26b56d30b4ce1b6aef2258fbb97
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-02-05 14:42:02 -0700

    worked on updated funancial calculator

---
+++ Practices/update_financial_calculator.py
@@ -0,0 +1,14 @@
+# IB 7th period Updated financial calculator
+
+def user_input(new_word):
+    money = round(float(input(f"What is your monthly {new_word}: $ ")), 2)
+    return money
+
+def percent():
+    percent = 
+
+user_input("income")
+user_input("rent")
+user_input("utilities")
+user_input("transportation")
+user_input("groceries")
