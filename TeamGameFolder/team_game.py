748078990fbeafcac3506ea2d719fd22e82408f7
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-02-06 14:42:04 -0700

    team game

--- TeamGameFolder/team_game.py
+++ TeamGameFolder/team_game.py
@@ -1,2 +1,21 @@
 #  IB GLG, 7th period, Adventure RPG Game - Improved Version
 
+# GLG = 🎀
+# IB = 
+
+import random
+
+#------------------
+# CONSTANTS 🎀
+#------------------
+
+MAX_INVENTORY = 7
+
+weapons = {
+"fists": .5,
+"stick": 2,
+"sword": 4,
+"magic wand":6
+}
+
+rooms = ["forest", "cave", "river", "temple"]
