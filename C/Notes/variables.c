b7d905a9e59b35097a60ab517e80b295bc4e1281
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-02-18 14:42:05 -0700

    \n new line!

--- C/Notes/variables.c
+++ C/Notes/variables.c
@@ -2,6 +2,16 @@
 #include <stdio.h> // Lets us use inputs and outputs
 
 int main(){
-    printf("It worked!");
+    //Variables examples
+    int age = 9999;
+    float gpa = 3.98;
+    char grade = 'A';   // Single letter uses single quotes
+    char name[] = "Eric";
+
+
+    printf("It worked!\n");
+    printf("%d\n", age);
+    printf("Your GPA is: %f\n", gpa);
+    printf("you have a %c incomputer science\n", grade);
     return 0; //required last line in your main function
 }
