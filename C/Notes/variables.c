e369492a892f014391cfabba8b212ee9946a5106
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-02-18 14:24:53 -0700

    Started code and settings for C

---
+++ C/Notes/variables.c
@@ -0,0 +1,7 @@
+// This is a comment in C!
+#include <stdio.h> // Lets us use inputs and outputs
+
+int main(){
+    printf("It worked!");
+    return 0; //required last line in your main function
+}
