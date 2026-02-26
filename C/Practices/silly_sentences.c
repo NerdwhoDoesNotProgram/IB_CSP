419096c8ef5c18535af39c02c94d494dd1e1ffa4
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-02-26 13:55:32 +0000

    Fixed silly sentences

--- C/Practices/silly_sentences.c
+++ C/Practices/silly_sentences.c
@@ -5,7 +5,7 @@
 int main(){
 	char first[20];
 	char last[50];
-	char full_name[20];
+	char full_name[70] = "";
 	char word1[20];
 	char word2[20];
 	char word3[20];
@@ -13,26 +13,26 @@ int main(){
 
 
 	printf("Please enter a First name: ");
-	scanf("%s", &first);
+	scanf("%s", first);
 
 	printf("Please enter a Last name: ");
-	scanf("%s", &last);
+	scanf("%s", last);
 
 	strcat(full_name, first);
 	strcat(full_name, " ");
 	strcat(full_name, last);
 
 	printf("Enter a noun: ");
-    scanf("%s", &word1);
+    scanf("%s", word1);
 
 	printf("Enter an adjective: ");
-    scanf("%s", &word2);
+    scanf("%s", word2);
 
 	printf("Enter a verb (past tense): ");
-    scanf("%s", &word3);
+    scanf("%s", word3);
 
     printf("Enter an adverb: ");
-    scanf("%s", &word4);
+    scanf("%s", word4);
 
 
 	printf("While visiting the spooky zoo, %s noticed a %s %s that %s %s into the bushes, leaving everyone feeling totally terrified.", full_name, word2, word1, word3, word4);
