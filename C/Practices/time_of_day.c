2358b07613f12bf474d9cc7b9cae7886508a1276
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-02-28 17:45:42 +0000

    Time of day is complete

--- C/Practices/time_of_day.c
+++ C/Practices/time_of_day.c
@@ -1,81 +1,45 @@
-// IB 7th Time of Day - C
+// IB 7th - Time of Day
 #include <stdio.h>
 #include <time.h>
-#include <stdlib.h>
 
-//int main(){
- //   time_t now = time(NULL);
- //   struct tm *t = localtime(&now);
- //   char buffer[100];
+int main() {
 
- //   strftime(buffer, sizeof(buffer), "%H:%M:%S", t);
-  //  printf("Formatted time: %s\n", buffer);
+    char answer;
+    int hour;
 
-  //  return 0;
-//}
-int time_input;
-int time_now;
-int current_hour_military;
+    printf("Do you know what time it is? (Y/N): ");
+    scanf(" %c", &answer);
 
+    if (answer == 'Y' || answer == 'y' || answer == 'yes' || answer == 'YES' || answer == 'Yes') {
 
-int time_of_day(){
-    time_t current_raw_time;
-    struct tm *day_time_info;
+        printf("What is the hour in military time (0-23): ");
+        scanf("%d", &hour);
 
-    // 1. Get the current time as a time_t value (seconds since Unix epoch)
-    current_raw_time = time(NULL);
-    if (current_raw_time == (time_t)-1) {
-        fprintf(stderr, "Failure to obtain the current time.\n");
-        return EXIT_FAILURE;
+    } 
+    else {
+
+        printf("Good thing I know...\n");
+
+        time_t now = time(NULL);
+        struct tm *local = localtime(&now);
+
+        hour = local->tm_hour;
+
+        printf("The current hour is: %d\n", hour);
     }
 
-    // 2. Convert the time_t value to local time and store in a struct tm
-    day_time_info = localtime(&current_raw_time);
-
-    // 3. Extract the hour (in 24-hour format) into your integer variable
-    // The tm_hour member stores hours since midnight in the range [0, 23].
-    current_hour_military = day_time_info->tm_hour;
-
-    // 4. Use the variable (optional: print to verify)
-    printf("The current hour in military time is: %d\n", current_hour_military);
-    // You can also use strftime to format the time into a string, if needed.
-
-    return EXIT_SUCCESS;
-}
-
-int unknown_time(){
-    time_now = 
-    printf("Good thing that I know...");
-    if(time_now)
-    return 0;
-}
-
-int known_time(){
-    if (time_input < 12){
-     printf("Good Morning!");
-
-    }else if (time_input >= 12 && time_input < 18){
-     printf("Good Afternoon!");
-    }else if (time_input < 0 || time_input > 24){
-        printf("That is not a time.");
-
-    }else{
-     printf("Good Evening!");
-
-    return 0;
+    if (hour < 0 || hour > 23) {
+        printf("That is not a valid time.\n");
     }
-}
-
-int main(){
-    char do_you_know = printf("Do you know what time it is (Yes/No): ");
-    scanf("%s", do_you_know);
-
-    if (do_you_know == "Yes"){
-        int what_time = printf("What is the hour in military time: ");
-        scanf("%d", what_time);
-        time_now = what_time;
-    } else{
-        printf("Good thing I know...");
+    else if (hour < 12) {
+        printf("Good Morning!\n");
     }
+    else if (hour < 18) {
+        printf("Good Afternoon!\n");
+    }
+    else {
+        printf("Good Evening!\n");
+    }
+
     return 0;
 }
