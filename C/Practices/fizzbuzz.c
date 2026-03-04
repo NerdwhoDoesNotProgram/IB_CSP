29b3083731379a5385937b98d9bac111f9ff629b
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-03-04 14:42:22 -0700

    FizzBuzz

---
+++ C/Practices/fizzbuzz.c
@@ -0,0 +1,16 @@
+// IB FizzBizz - C
+#include <stdio.h>
+
+int main(){
+    for(int i = 1; i < 51; i++){
+        if(i % 15 == 0);
+            printf("FizzBuzz");
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+    else:
+        print(i)
+    }
+    return 0;
+}
