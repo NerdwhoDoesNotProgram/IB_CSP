bde7a6e809bd058161ac638c814e7a0e633e0fd0
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-03-05 14:39:19 -0700

    fizzbuzz

--- C/Practices/fizzbuzz.c
+++ C/Practices/fizzbuzz.c
@@ -5,14 +5,11 @@ int main(){
     for(int i = 1; i < 51; i++){
         if(i % 15 == 0){
             printf("FizzBuzz\n");
-        }
-        else if(i % 3 == 0){
+        }else if(i % 3 == 0){
             printf("Fizz\n");
-        }
-        else if(i % 5 == 0){
+        }else if(i % 5 == 0){
             printf("Buzz\n");
-        }
-        else{
+        }else{
             printf("%d\n", i);
         }
     }
