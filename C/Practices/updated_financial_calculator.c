63521b78d404ee162538cc692c88b7ea4be7792b
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-03-08 06:32:39 +0000

    Final update to financial calculator

--- C/Practices/updated_financial_calculator.c
+++ C/Practices/updated_financial_calculator.c
@@ -1,6 +1,7 @@
 // IB 7th Update Financial Calculator
 #include <stdio.h>
 
+/*
 // Function to get user input
 float get_input(char prompt[]) {
     float value;
@@ -47,51 +48,120 @@ int main() {
     }
 
     return 0;
-}
+} */
 
+// Verision 2
 /*
-// Function to get user input
-float get_input(char prompt[]) {
+// get money input
+float get_money(char prompt[]) {
     float value;
     printf("%s", prompt);
     scanf("%f", &value);
     return value;
 }
 
-// Function to calculate percent of income
-float percent_of_income(float income, float expense) {
+// calculate percent of income
+float get_percent(float income, float expense) {
     return (expense / income) * 100;
 }
 
+// print category info
+void print_category(char name[], float income, float amount) {
+    float percent = get_percent(income, amount);
+    printf("Your %s is $%.2f and that is %.0f%% of your income.\n",
+           name, amount, percent);
+}
+
 int main() {
-      char expenses[][20] = {"rent", "utilities", "groceries", "transportation"};
 
-      char amounts;
+    float income = get_money("What is your monthly income: $ ");
 
-       float income = get_input("What is your monthly income: $");
-       float rent = get_input("What is your monthly rent/mortgage: $");
-       float utilities = get_input("What is your monthly utilities: $");
-       float groceries = get_input("What is your monthly groceries: $");
-       float transportation = get_input("What is your monthly transportation: $");
+    char *expenses[] = {"rent", "utilities", "groceries", "transportation"};
+    float amounts[4];
 
-       float savings = income * 0.10;
-       float total_expenses = rent + utilities + groceries + transportation;
-       float spending_money = income - total_expenses - savings;
+    // get expense inputs
+    for(int i = 0; i < 4; i++) {
+        char prompt[100];
+        printf("What is your monthly %s: $ ", expenses[i]);
+        scanf("%f", &amounts[i]);
+    }
 
-       printf("\nYour rent is $%.2f and that is %.0f%% of your income.\n",
-           rent, percent_of_income(income, rent));
+    float savings_percent = get_money("What percentage of your income would you like to save? ");
 
-       printf("Your utilities are $%.2f and that is %.0f%% of your income.\n",
-           utilities, percent_of_income(income, utilities));
+    float savings = income * (savings_percent / 100);
 
-       printf("Your groceries are $%.2f and that is %.0f%% of your income.\n",
-           groceries, percent_of_income(income, groceries));
+    // print expense info
+    for(int i = 0; i < 4; i++) {
+        print_category(expenses[i], income, amounts[i]);
+    }
 
-       printf("Your transportation is $%.2f and that is %.0f%% of your income.\n",
-           transportation, percent_of_income(income, transportation));
+    print_category("savings", income, savings);
 
-       printf("You should save $%.2f a month, that is 10%% of your income.\n", savings);
-       printf("You have $%.2f of spending money each month!\n", spending_money);
+    // calculate total expenses
+    float total_expenses = savings;
+    for(int i = 0; i < 4; i++) {
+        total_expenses += amounts[i];
+    }
+
+    float spending = income - total_expenses;
+
+    if(spending >= 0) {
+        printf("You have $%.2f of spending money each month!\n", spending);
+    } else {
+        printf("You are $%.2f in debt each month!\n", spending * -1);
+    }
 
     return 0;
-}*/
+} */
+
+// Version 3
+
+float get_money(char prompt[]) {
+    float value;
+    printf("%s", prompt);
+    scanf("%f", &value);
+    return value;
+}
+
+float get_percent(float income, float expense) {
+    return (expense / income) * 100;
+}
+
+void print_category(char name[], float income, float amount) {
+    printf("Your %s is $%.2f and that is %.0f%% of your income.\n",
+           name, amount, get_percent(income, amount));
+}
+
+int main() {
+
+    float income = get_money("What is your monthly income: $ ");
+
+    char *expenses[] = {"rent", "utilities", "groceries", "transportation"};
+    float amounts[4];
+
+    for (int i = 0; i < 4; i++) {
+        printf("What is your monthly %s: $ ", expenses[i]);
+        scanf("%f", &amounts[i]);
+    }
+
+    float savings_percent = get_money("What percentage of your income would you like to save? ");
+    float savings = income * (savings_percent / 100);
+
+    float total_expenses = savings;
+
+    for (int i = 0; i < 4; i++) {
+        print_category(expenses[i], income, amounts[i]);
+        total_expenses += amounts[i];
+    }
+
+    print_category("savings", income, savings);
+
+    float spending = income - total_expenses;
+
+    if (spending >= 0)
+        printf("You have $%.2f of spending money each month!\n", spending);
+    else
+        printf("You are $%.2f in debt each month!\n", -spending);
+
+    return 0;
+}
