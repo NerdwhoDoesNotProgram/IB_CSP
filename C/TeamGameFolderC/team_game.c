753585ea73ee8f3dc3ade62e81f1d8ddee6646a0
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-03-16 14:42:29 -0600

    Co-authored-by: MangoMuncher346 <MangoMuncher346@users.noreply.github.com>

--- C/TeamGameFolderC/team_game.c
+++ C/TeamGameFolderC/team_game.c
@@ -1,5 +1,46 @@
 // IB RH, 7th period, Adventure RPG Game
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <time.h>
+
+const int max_inventory = 5;
+
+char weapons[][4] = {
+    "fists": 1,
+    "stick": 2,
+    "sword": 3,
+    "magic wand": 4,
+};
+
+char rooms[][7] = {"forest", "cave", "river", "temple"};
+
+// -----------------------------------
+ //RESET GAME | Written by RH | Edited by
+// -----------------------------------
+
+// This is how we reset game if you die.
+
+int health;
+int gold;
+char inventory[5]; 
+int current_room;
+int game_running;
+
+int reset_game();
+
+health = 12;
+gold = 0;
+inventory[0] = " ";
+current_room = "forest";
+game_running = true;
+
+// -----------------------------------
+// INSTRUCTIONS | Written by 😊 | Edited by
+// -----------------------------------
+  
+// Opening title page! Shows you the commands for the game.
+
+int show_instructions();
+   
 
